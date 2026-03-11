cdde5edbc241aafbe9cd028d56daed1754c19997
perryjc <perryjeff366@gmail.com>
2026-03-11 08:18:07 -0400

    death shaft

--- projects/game/game.js
+++ projects/game/game.js
@@ -8,11 +8,43 @@ let minutes = 0;
 
 //Declare your other global variables here
 
+//Example function:
+/* function CreekBed() {
+    clear();
+    print("\nYou are in a creek bed");
+    print("\nDiscription of enviremnet");
+    print("\nWhat do you do? Say one of the options:" +
+        "\n\tOption1" +
+        "\n\tOption2" +
+        "\n\tOption3" +
+        "\n\tBackpack");
+    
+    function processInput(input){
+        if (input.toLowerCase() === "option1") {
+            leftT();
+        } else if (input.toLowerCase() === "option2") {
+            middleT();
+        } else if (input.toLowerCase() === "option3") {
+            rightT();
+        } else if (input.toLowerCase() === "backpack") {
+            Backpack(CreekBed);
+        } else {
+            stayHere();
+            waitThenCall(CreekBed);
+        }
+    }
+    waitForInput(processInput);
+} */
 
 //If you need, add any "helper" functions here
 
 
 //Make one function for each location
+
+//TODO:
+//Add a line by line loading method for the title screen.
+//Intigrate emotion and cool factor
+//Add time system
 function locationA() {
     clear();
     print("\nYou are on the Bike Trail");
@@ -76,6 +108,11 @@ function end1() {
     print("\n\tWas there more? What did I miss? Can I go back?") 
     print("\n\tno.");
     print("\n\tTHE END");
+    print("\n\tPress any key to start over.")
+    function processInput(input){
+            window.location.reload();
+    }
+    waitForInput(processInput);
 }
 function CreekBed() {
     clear();
@@ -108,33 +145,49 @@ function leftT() {
     clear();
     print("\nYou are in the Left tunnel");
     if (flashLightState == false) {
-            print("\n\tYou gase into the nauseating darkness" +
-                "\n\tYou hear the rush off water. theres nothing for you"+
-                "\n\tit is to dark. if only there was a way to see in the dark"+
-                "\n\tGo back");
+            print("\nYou gaze into the nauseating darkness" +
+                "\nYou hear the rush off water. theres nothing for you"+
+                "\nIt is to dark. if only there was a way to see in the dark");
+            print("\nPress any button to go back");
             function processInput(input){
-                if (input.toLowerCase() === "go back") {
-                    CreekBed();
-                } else {
-                    stayHere();
-                    waitThenCall(leftT);
-                }
-            } 
+                        CreekBed();
+            }
+            waitForInput(processInput);
         } else {
-        //ended here march 9
-        print("\n\tYou step into the tunnel and see walls covered in grafitti" +
-            "\n\tThere are two paths that look the same");
+        print("\nYou step into the tunnel and see walls covered in grafitti" +
+            "\nThere are two paths that look the same");
         print("\nWhere do you want to go next? Say one of these choices:" +
         "\n\tLeft path" +
         "\n\tRight path" + 
-        "\n\tTag the walls");
+        "\n\tTag the walls" +
+        "\n\tBackpack");
         function processInput(input){
             if (input.toLowerCase() === "left path") {
-                locationA();
+                print("\nYou take the left path, your first step goes into ankel deep water" +
+                    "\nYour shoe squeaks and squelches with every step" +
+                    "\nYou are uncomfortable"
+                );
+                emotionNow = emotionNow - 1;
+                print("press any key to continue");
+                function processInput(input){
+                        Longtunnel1();
+                }
+                waitForInput(processInput);
             } else if (input.toLowerCase() === "right path") {
-                locationB();
+                Longtunnel1();
             } else if (input.toLowerCase() === "tag the walls") {
-                tagWalls();
+                print("\nYou take out the can of red spray paint from your bag and remove the cap" +
+                    "\nYou begin to mark the walls" +
+                    "\nYou feel pretty cool"
+                );
+                coolFactor = coolFactor + 10;
+                print("press any key to continue");
+                function processInput(input){
+                        Longtunnel1();
+                }
+                waitForInput(processInput);
+            } else if (input.toLowerCase() === "backpack") {
+                Backpack(leftT);
             } else {
                 stayHere();
                 waitThenCall(leftT);
@@ -142,6 +195,74 @@ function leftT() {
         }
         waitForInput(processInput);
     }
+}
+
+function Longtunnel1() {
+    clear();
+    print("\nYou are in a long tunnel");
+    print("\nYou walk through the shallow water, walls covered in swastikas and naked women, huge crickets crawl along the moist concrete." +
+        "\nYou reach a point where a small tunnel splits off from the main one."
+    );
+    print("\nWhat do you do? Say one of the options:" +
+        "\n\tContinue walking" +
+        "\n\tCheck out the tunnel" +
+        "\n\tBackpack");
+    
+    function processInput(input){
+        if (input.toLowerCase() === "continue walking") {
+            Junction1();
+        } else if (input.toLowerCase() === "check out the tunnel") {
+            verticalShaft();
+        }  else if (input.toLowerCase() === "backpack") {
+            Backpack(Longtunnel1);
+        } else {
+            stayHere();
+            waitThenCall(Longtunnel1);
+        }
+    }
+    waitForInput(processInput);
+}
+
+function verticalShaft1() {
+    clear();
+    print("\nYou are in the Offshoot");
+    print("\nYou crawl into the small tunnel. As you look up you see a shaft extend 50 feet above you." +
+        "\nThere are rusty ladder rungs embedded in the walls" +
+        "\nYou think you see another tunnel that seems only accessable from the top of the shaft."
+    );
+    //ended march 10th
+    print("\nWhat do you do? Say one of the options:" +
+        "\n\tGo back" +
+        "\n\tClimb the ladder" +
+        "\n\tBackpack");
+    
+    function processInput(input){
+        if (input.toLowerCase() === "Go back") {
+            Longtunnel1();
+        } else if (input.toLowerCase() === "climb the ladder") {
+            verticalShaftEnd();
+        }  else if (input.toLowerCase() === "backpack") {
+            Backpack(verticalShaft1);
+        } else {
+            stayHere();
+            waitThenCall(verticalShaft1);
+        }
+    }
+    waitForInput(processInput);
+}
+
+function verticalShaftEnd() {
+    clear();
+    print("\nYou begin to climb the slick rungs."); 
+    print("\nYour arms are shaking as you struggle to keep steady");
+    print("\nSuddenly, you slip. Your hands attempt to grab around nothing as you fall down the shaft.")
+    print("\nYour head hits the ground with a crack, and everything goes black."); 
+    print("\nTHE END");
+    print("\nPress any key to start over.")
+    function processInput(input){
+            window.location.reload();
+    }
+    waitForInput(processInput);
 }
 
 //finally, make sure you customize this to tell it what should happen at the
@@ -158,7 +279,7 @@ function start(){
                 \/         \/                 \/       \/                   \/        \/          \/                \/ 
  
 `);
-
+    print("press any key to start");
     function processInput(input){
             locationA();
     }
