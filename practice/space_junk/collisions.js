a3a06dea14cabbae5308bfeb57ee5c5e4e44ec14
perryjc <perryjeff366@gmail.com>
2026-04-08 09:37:00 -0400

    cats lmafa

--- practice/space_junk/collisions.js
+++ practice/space_junk/collisions.js
@@ -1,6 +1,8 @@
 function clickHandler(e){
+    //compinsates for canvas
   const mouseX = e.offsetX, mouseY = e.offsetY;
   for(const j of junk) {
+    //bounding box
     junk_min_x = j.x - 12*j.size;
     junk_max_x = j.x + 12*j.size;
     junk_min_y = j.y - 12*j.size;
@@ -8,6 +10,12 @@ function clickHandler(e){
 
     //TODO: write some code here that checks whether
     //(mouseX, mouseY) is inside j
+    //Below is attack function.
+    console.log(mouseX, mouseY);
+    if(mouseX > junk_min_x && mouseX < junk_max_x && mouseY > junk_min_y && mouseY < junk_max_y){
+        console.log("Succssful hit");
+        j.alive = false;
+    }
 
   }
 }
@@ -21,6 +29,7 @@ function checkCollisions(){
     for(const j of junk) {
         for(const s of ships){
             if(j.alive && s.alive){
+                //bounding box
                 junk_min_x = j.x - 12*j.size;
                 junk_max_x = j.x + 12*j.size;
                 junk_min_y = j.y - 12*j.size;
@@ -31,6 +40,12 @@ function checkCollisions(){
                 ship_min_y = s.y - 15*s.size;
                 ship_max_y = s.y + 15*s.size;
 
+                //Ship hits junk function:
+                if(junk_min_x < ship_max_x && junk_max_x > ship_min_x && junk_min_y < ship_max_y && junk_max_y > ship_min_y){
+                    console.log("Collision");
+                    j.alive = false;
+                    s.alive = false;
+                }
             }
         }
     }
